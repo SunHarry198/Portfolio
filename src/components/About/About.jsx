b88d947600a62a1249d6236e3922a47c36761b18
Hung <hungdz.it@gmail.com>
2022-05-22 02:07:31 +0700

    first update portfolio

---
+++ src/components/About/About.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './about.style.scss'
+import plance1 from 'src/assets/images/plance-1.png'
+function About() {
+  return (
+    <div className="about scroll-section hidden" id="about-nav">
+      <div className="about__left">
+        <div className="about__left--image">
+          <img src={plance1} alt="plance" />
+        </div>
+      </div>
+      <div className="about__right">
+        <section>
+          <div className="about__right--title">About Me</div>
+          <div className="about__right--content">
+            <div>
+              Good morning, everyone! I'm Tran Van Hung, I'm 24 years old and
+              I'm from DakLak. I'm Fresher Front-end Developer. I have six
+              months experience of working as a web developer at HCMC.
+            </div>
+            <div>
+              I'm often praised for my creativity and independent working
+              skills. I always put my heart and soul into every single website
+              that I develop. I guess that's all I have to share about myself.
+              Thank you for reading!
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './header.style.scss'
+import CV from 'src/assets/images/TRAN-VAN-HUNG-FRESHER-FRONT-END.pdf'
+
+function Header() {
+  return (
+    <div className="header scroll-section" id="header-nav">
+      {/* <div className="header__plance">
+        
+      </div> */}
+      <div className="header__left">
+        <div className="header__left--content">
+          <div className="header__left--content--title">Hello, I'm Hung</div>
+          <article className="header__left--content--fe">
+            Front-End Developer
+          </article>
+          <section className="header__left--content--button">
+            <a href={CV} download>
+              <article className="btn btn__download cursor_small">
+                Download CV
+              </article>
+            </a>
+            <a href="#contact-nav" className="btn btn__let-talk cursor_small">
+              Let's Talk
+            </a>
+          </section>
+        </div>
+      </div>
+      <div className="header__right">
+        <div className="header__right--image"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
